b40d33254421a5e1a70fc56d65bc5ae83005196f
Giuseppe Gerbino <giu.gerbino@gmail.com>
2020-01-26 23:26:39 +0100

    Create exercise

--- src/App.js
+++ src/App.js
@@ -11,11 +11,12 @@ function App() {
   return (
     <Router>
       <Navbar />
-      <br/>
-      <Route path="/" exact component={ExercisesList} />
-      <Route path="/edit/:id" component={EditExercise} />
-      <Route path="/create" component={CreateExercise} />
-      <Route path="/user" component={CreateUser} />
+      <div className="container">
+        <Route path="/" exact component={ExercisesList} />
+        <Route path="/edit/:id" component={EditExercise} />
+        <Route path="/create" component={CreateExercise} />
+        <Route path="/user" component={CreateUser} />
+      </div>
     </Router>
   );
 }
